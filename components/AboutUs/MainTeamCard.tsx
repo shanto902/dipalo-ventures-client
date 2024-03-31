c3419a2301674165d59c36d1ca39e4dbd7b7f171
Ashik Ali Shanto <shanto902@gmail.com>
2024-03-31 07:40:13 +0600

    card created

--- components/AboutUs/MainTeamCard.tsx
+++ components/AboutUs/MainTeamCard.tsx
@@ -15,20 +15,30 @@ const MainTeamCard = ({
   linkedinLink?: string;
 }) => {
   return (
-    <div className="relative w-full h-full">
-      <div className="relative w-full h-full">
-        <Image
-          className="object-cover"
-          src={image}
-          alt={name}
-          height={500}
-          width={400}
-        />
-        <div className="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-70">
-          <div className="absolute bottom-0 left-0 w-full text-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
-            <h3 className="text-white">{name}</h3>
-            <h4 className="text-gray-400">{designation}</h4>
-            {bio && <p className="text-gray-300">{bio}</p>}
+    <div>
+      <div className="group relative cursor-pointer items-center justify-center overflow-hidden  ">
+        <div className=" aspect-[3/4] ">
+          <Image
+            className="h-full w-full object-cover  transition-transform duration-500 "
+            src={image}
+            alt=""
+            width={500}
+            height={1000}
+          />
+        </div>
+        <div className="absolute inset-0"></div>
+        <div className="absolute inset-0 flex translate-y-[40%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+          <div className="  bg-black bg-opacity-70 w-[363px] h-[110px]  group-hover:opacity-0 transition-opacity duration-300">
+            <h2 className="text-center mt-5 text-white text-base font-bold uppercase">
+              Rafiq Ahmed
+            </h2>
+            <h3 className="text-center text-amber-500 text-xs font-semibold mt-2">Managing Partner</h3>
+          </div>
+          <div className=" absolute h-full w-full text-left   text-white text-xs font-semibold opacity-0 transition-opacity  bg-black bg-opacity-70 duration-300 group-hover:opacity-100 p-5 overflow-y-scroll">
+         {bio}
+            <button className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+              See More
+            </button>
           </div>
         </div>
       </div>
--- components/AboutUs/TeamSection.tsx
+++ components/AboutUs/TeamSection.tsx
@@ -4,9 +4,9 @@ import MainTeamCard from './MainTeamCard'
 
 const TeamSection = () => {
   return (
-    <PaddingContainer className=' grid grid-cols-4 gap-5 '>
+    <PaddingContainer className=' my-5 grid grid-cols-4 gap-10 '>
         {/* Titles  */}
-        <div className=' col-span-2 flex flex-col gap-5 py-20'> 
+        <div className=' col-span-2 flex flex-col justify-center gap-5'> 
         <div className="text-dipalo text-5xl font-bold  leading-[84.44px] underline underline-offset-8 decoration-dipalo">THE TEAMS</div>
         <div className="text-black text-5xl font-bold ">We Help To Achieve<br/>Your Business Goal</div>
         <div className="w-[592px] h-[78px] text-zinc-900 text-2xl font-normal  leading-[37px]">Collectively, we represent a level of experience in product development and manufacturing generally not found in venture capital.</div>
@@ -14,6 +14,30 @@ const TeamSection = () => {
 
         {/* Main Team  */}
         <MainTeamCard image={'/assets/about-us/rafiq.png'} name='RAFIQ AHMED' designation='Managing Partner' bio='Lauren is a Managing Partner at Dipalo Ventures. She works to raise and effectively deploy the fund’s capital into top-flight companies. She sources high-quality startups, assists with the Dipalo Ventures Residency, and performs due diligence to ensure a high-quality portfolio.
+
+Lauren has over 20 years of startup investment, capacity building, and mentorship. She is an active LP, serial entrepreneur, and angel investor. Her focus areas include energy, innovative materials, IT, Fintech, and B2C fields. Previously, Lauren screened thousands of high-growth technology companies in her role as the Founding Managing Director of the Cleantech Group and Executive Director at Accelerate Michigan. She is also an active board member of the Michigan Department of Treasury’s Board of Financial Authority.
+
+Lauren received her MA and Ph.D. from Northwestern University and her BA from the University of Michigan.'  />
+  <MainTeamCard image={'/assets/about-us/rafiq.png'} name='RAFIQ AHMED' designation='Managing Partner' bio='<p>Lauren is a Managing Partner at Dipalo Ventures. She works to raise and effectively deploy the fund’s capital into top-flight companies. She sources high-quality startups, assists with the Dipalo Ventures Residency, and performs due diligence to ensure a high-quality portfolio.
+Lauren has over 20 years of startup investment, capacity building, and mentorship. She is an active LP, serial entrepreneur, and angel investor. Her focus areas include energy, innovative materials, IT, Fintech, and B2C fields. Previously, Lauren screened thousands of high-growth technology companies in her role as the Founding Managing Director of the Cleantech Group and Executive Director at Accelerate Michigan. She is also an active board member of the Michigan Department of Treasury’s Board of Financial Authority.
+
+Lauren received her MA and Ph.D. from Northwestern University and her BA from the University of Michigan.</p>'  />
+  <MainTeamCard image={'/assets/about-us/rafiq.png'} name='RAFIQ AHMED' designation='Managing Partner' bio='Lauren is a Managing Partner at Dipalo Ventures. She works to raise and effectively deploy the fund’s capital into top-flight companies. She sources high-quality startups, assists with the Dipalo Ventures Residency, and performs due diligence to ensure a high-quality portfolio.
+
+Lauren has over 20 years of startup investment, capacity building, and mentorship. She is an active LP, serial entrepreneur, and angel investor. Her focus areas include energy, innovative materials, IT, Fintech, and B2C fields. Previously, Lauren screened thousands of high-growth technology companies in her role as the Founding Managing Director of the Cleantech Group and Executive Director at Accelerate Michigan. She is also an active board member of the Michigan Department of Treasury’s Board of Financial Authority.
+
+Lauren received her MA and Ph.D. from Northwestern University and her BA from the University of Michigan.'  />
+  <MainTeamCard image={'/assets/about-us/rafiq.png'} name='RAFIQ AHMED' designation='Managing Partner' bio='Lauren is a Managing Partner at Dipalo Ventures. She works to raise and effectively deploy the fund’s capital into top-flight companies. She sources high-quality startups, assists with the Dipalo Ventures Residency, and performs due diligence to ensure a high-quality portfolio.
+
+Lauren has over 20 years of startup investment, capacity building, and mentorship. She is an active LP, serial entrepreneur, and angel investor. Her focus areas include energy, innovative materials, IT, Fintech, and B2C fields. Previously, Lauren screened thousands of high-growth technology companies in her role as the Founding Managing Director of the Cleantech Group and Executive Director at Accelerate Michigan. She is also an active board member of the Michigan Department of Treasury’s Board of Financial Authority.
+
+Lauren received her MA and Ph.D. from Northwestern University and her BA from the University of Michigan.'  />
+  <MainTeamCard image={'/assets/about-us/rafiq.png'} name='RAFIQ AHMED' designation='Managing Partner' bio='Lauren is a Managing Partner at Dipalo Ventures. She works to raise and effectively deploy the fund’s capital into top-flight companies. She sources high-quality startups, assists with the Dipalo Ventures Residency, and performs due diligence to ensure a high-quality portfolio.
+
+Lauren has over 20 years of startup investment, capacity building, and mentorship. She is an active LP, serial entrepreneur, and angel investor. Her focus areas include energy, innovative materials, IT, Fintech, and B2C fields. Previously, Lauren screened thousands of high-growth technology companies in her role as the Founding Managing Director of the Cleantech Group and Executive Director at Accelerate Michigan. She is also an active board member of the Michigan Department of Treasury’s Board of Financial Authority.
+
+Lauren received her MA and Ph.D. from Northwestern University and her BA from the University of Michigan.'  />
+  <MainTeamCard image={'/assets/about-us/rafiq.png'} name='RAFIQ AHMED' designation='Managing Partner' bio='Lauren is a Managing Partner at Dipalo Ventures. She works to raise and effectively deploy the fund’s capital into top-flight companies. She sources high-quality startups, assists with the Dipalo Ventures Residency, and performs due diligence to ensure a high-quality portfolio.
 
 Lauren has over 20 years of startup investment, capacity building, and mentorship. She is an active LP, serial entrepreneur, and angel investor. Her focus areas include energy, innovative materials, IT, Fintech, and B2C fields. Previously, Lauren screened thousands of high-growth technology companies in her role as the Founding Managing Director of the Cleantech Group and Executive Director at Accelerate Michigan. She is also an active board member of the Michigan Department of Treasury’s Board of Financial Authority.
 
