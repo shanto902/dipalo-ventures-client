3bceec87a71cd6289de28aace916f7d253869e02
Ashik Ali Shanto <shanto902@gmail.com>
2024-04-24 17:24:34 +0600

    Team section fixed v5

--- lib/directus.ts
+++ lib/directus.ts
@@ -3,7 +3,7 @@ const directus = createDirectus(process.env.NEXT_PUBLIC_API_URL as string)
   .with(staticToken(process.env.ACCESS_TOKEN as string))
   .with(
     rest({
-      onRequest: (options) => ({ ...options, cache: 'no-store' }),
+      onRequest: (options) => ({ ...options }),
     })
   );
 
