1997de851825d0e1a41ca3268df62667df947d66
Shanto <ashikalishanto@gmail.com>
2024-05-10 05:32:36 +0600

    contact page optimize

--- components/Pages/AboutUs/HeaderAboutUs.tsx
+++ components/Pages/AboutUs/HeaderAboutUs.tsx
@@ -1,14 +1,11 @@
-
 import Image, { StaticImageData } from 'next/image';
 import React from 'react';
 import PaddingContainer from '../../Layout/PaddingContainer';
-
 
 import Typewriting from '@/components/common/Typewriting';
 import AnimatedDiv from '@/components/common/AnimatedDiv';
 
 const HeaderAboutUs = ({ image }: { image: StaticImageData }) => {
-
   return (
     <div className="relative md:h-[455px] h-[60vh] overflow-hidden">
       <Image
@@ -18,18 +15,22 @@ const HeaderAboutUs = ({ image }: { image: StaticImageData }) => {
         src={image}
         fill
       />
-      <PaddingContainer className="absolute inset-0 flex flex-col z-10 items- justify-center text-white px-5 mt-44">
-        <Typewriting className='text-white text-3xl leading-[76px] font-bold drop-shadow-2xl'   text='Dipalo [DI-PA-LO]'/>
-        <AnimatedDiv animationStyle='down' delay={2} >
-        <ol className="list-decimal pl-5 lg:text-lg md:text-base font-normal leading-loose text-white ">
-          <li>
-            From Bangla, the modern South Asian language rooted in Sanskrit
-          </li>
-          <li>
-            ‘Pra-dip’ (Bangla), ‘Pra-di-pa’ (Sanskrit) meaning “light, lantern”
-          </li>
-          <li>‘Alo’ meaning light</li>
-        </ol>
+      <PaddingContainer className="absolute inset-0 flex flex-col z-10 items- justify-center text-white px-5 md:mt-44 mt-10 ">
+        <Typewriting
+          className="text-white text-3xl leading-[76px] font-bold drop-shadow-2xl"
+          text="Dipalo [DI-PA-LO]"
+        />
+        <AnimatedDiv animationStyle="down" delay={2}>
+          <ol className="list-decimal pl-5 lg:text-lg md:text-base font-normal leading-loose text-white ">
+            <li>
+              From Bangla, the modern South Asian language rooted in Sanskrit
+            </li>
+            <li>
+              ‘Pra-dip’ (Bangla), ‘Pra-di-pa’ (Sanskrit) meaning “light,
+              lantern”
+            </li>
+            <li>‘Alo’ meaning light</li>
+          </ol>
         </AnimatedDiv>
       </PaddingContainer>
       <div className="absolute inset-0 bg-black opacity-70" />
--- components/Pages/Contact/ContactCard.tsx
+++ components/Pages/Contact/ContactCard.tsx
@@ -1,6 +1,7 @@
+import CustomTitle from '@/components/common/CustomTitle';
 import React, { ReactNode } from 'react';
 import { FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa6';
-
+import parse from 'html-react-parser';
 const ContactCard = ({
   mainText,
   subText,
@@ -13,30 +14,49 @@ const ContactCard = ({
   isSocial?: boolean;
 }) => {
   return (
-    <div className="w-[420px] h-[236.33px] relative">
-      <div className="w-[390px] h-[206.33px] left-[15px] top-[30px] absolute bg-white rounded-[10px] border border-gray-200">
-        <div className="w-[132.36px] h-[25.33px] left-[41px] top-[55px] absolute text-neutral-800 text-lg font-bold  leading-relaxed">
-          {mainText}
-        </div>
-        <div className="w-[211px] left-[41px] top-[93px] absolute text-zinc-900 text-opacity-75 text-base font-semibold ">
-          {isSocial ? (
-            <div className=" flex gap-5 text-4xl text-black">
-              {' '}
-              <a target='_blank' href="https://www.linkedin.com/company/dipaloventures/">
-                <FaLinkedin className="hover:text-dipalo cursor-pointer transition-colors duration-300" />{' '}
-              </a>
-              <a target='_blank' href="https://twitter.com/DipaloVentures">
-                <FaTwitter className="hover:text-dipalo cursor-pointer transition-colors duration-300" />
-              </a>{' '}
-            <a target='_blank' href="https://www.instagram.com/dipaloventures/">  <FaInstagram className="hover:text-dipalo cursor-pointer transition-colors duration-300" /></a>{' '}
-              <a target='_blank' href="https://www.youtube.com/@dipaloventures9243"><FaYoutube className="hover:text-dipalo cursor-pointer transition-colors duration-300" /></a>{' '}
-            </div>
-          ) : (
-            subText
-          )}
+    <div className="w-72 h-60 relative">
+      <div className="w-full h-full left-0 top-8 absolute bg-white rounded-[10px] border border-gray-200 flex flex-col items-baseline">
+        <div className=" absolute top-10 mx-5">
+          <CustomTitle className=" mb-5 text-base text-left">
+            {mainText}
+          </CustomTitle>
+          <div className=" text-base font-semibold  ">
+            {isSocial ? (
+              <div className=" flex gap-5 text-4xl text-black">
+                {' '}
+                <a
+                  target="_blank"
+                  href="https://www.linkedin.com/company/dipaloventures/"
+                >
+                  <FaLinkedin className="hover:text-dipalo cursor-pointer transition-colors duration-300" />{' '}
+                </a>
+                <a target="_blank" href="https://twitter.com/DipaloVentures">
+                  <FaTwitter className="hover:text-dipalo cursor-pointer transition-colors duration-300" />
+                </a>{' '}
+                <a
+                  target="_blank"
+                  href="https://www.instagram.com/dipaloventures/"
+                >
+                  {' '}
+                  <FaInstagram className="hover:text-dipalo cursor-pointer transition-colors duration-300" />
+                </a>{' '}
+                <a
+                  target="_blank"
+                  href="https://www.youtube.com/@dipaloventures9243"
+                >
+                  <FaYoutube className="hover:text-dipalo cursor-pointer transition-colors duration-300" />
+                </a>{' '}
+              </div>
+            ) : (
+              <div className="rich-text">{parse(subText as string)}</div>
+            )}
+          </div>
         </div>
       </div>
-      <div className="w-[78px] h-[78px] left-[47px] top-[-13px] flex justify-center items-center absolute bg-white rounded-[78px] border border-gray-200">
+      <div
+        className="size-16
+       left-10 -top-3 flex justify-center items-center absolute bg-white rounded-full border border-gray-200"
+      >
         {children}
       </div>
     </div>
